379a1dba2a3cee8b16f9d483c514d04584672097
mcull <marc.cull@gmail.com>
2024-03-21 10:45:52 -0700

    clean up code

--- scripts/dalle.js
+++ scripts/dalle.js
@@ -1,11 +1,7 @@
 import { OpenAI } from 'openai';
 import { image as downloadImage } from 'image-downloader';
 import filenamify from 'filenamify';
-import { readFileSync, writeFileSync } from 'fs';
-import dayjs from 'dayjs';
-import fetch from 'node-fetch';
-import fs from 'fs';
-import { humanId, poolSize, minLength, maxLength } from 'human-id';
+import { humanId} from 'human-id';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -27,42 +23,33 @@ if (process.argv.length < 3) {
     process.exit(1);
 }
 
-await drawThing(prompt, imageName, layout);
+try {  
+    const imageFile = imageName || humanId();
+    // Acceptable Sizes: ['256x256', '512x512', '1024x1024', '1024x1792', '1792x1024'
+    const SQUARE = '1024x1024';
+    const TALL = '1024x1792';
+    const WIDE = '1792x1024';
 
-async function drawThing(prompt, size) {
+    let size = TALL;
+    if (layout === 'WIDE') {
+        size = WIDE;
+    } else if (layout === 'SQUARE') {
+        size = SQUARE;
+    }
 
-   // async () => {
-        try {  
-          const imageFile = imageName || humanId();
-            // Acceptable Sizes: ['256x256', '512x512', '1024x1024', '1024x1792', '1792x1024'
-            const SQUARE = '1024x1024';
-            const TALL = '1024x1792';
-            const WIDE = '1792x1024';
-
-            let size = TALL;
-            if (layout === 'WIDE') {
-                size = WIDE;
-            } else if (layout === 'SQUARE') {
-                size = SQUARE;
-            }
-
-            console.log("sending prompt to dalle: " + prompt);
-            let image = await openai.images.generate({ 
-                model: "dall-e-3",
-                prompt: prompt,
-                n: 1,
-                size: size
-            });
-            let revisedPrompt = image.data[0].revised_prompt
-            let url = image.data[0].url;
-            console.log(`got image ${url}`);
-            const filename = `${__dirname}/images/${filenamify(imageName + '.png')}`;
-            console.log(filename);
-            downloadImage({'url':url, 'dest': filename}).then(({ filename }) => {
-                console.log('Saved to', filename);
-            }).catch((err) => console.error(err));
-        } catch (openAIError) { 
-            console.log(openAIError);
-        }
-   //}
+    console.log("sending prompt to dalle: " + prompt);
+    let image = await openai.images.generate({ 
+        model: "dall-e-3",
+        prompt: prompt,
+        n: 1,
+        size: size
+    });
+    let revisedPrompt = image.data[0].revised_prompt
+    let url = image.data[0].url;
+    const filename = `${__dirname}/images/${filenamify(imageName + '.png')}`;
+    downloadImage({'url':url, 'dest': filename}).then(({ filename }) => {
+        console.log('Success. Image generated and saved to', filename);
+    }).catch((err) => console.error(err));
+} catch (openAIError) { 
+    console.log(openAIError);
 }
